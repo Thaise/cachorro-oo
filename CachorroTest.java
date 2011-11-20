d52079dd730cfa6576a25a03c9ea0bd6a68719b2
Thaise <thaisefloripa@gmail.com>
2011-11-20 17:08:55 -0200

    Testes - orientação a objetos - cachorro

--- CachorroTest.java
+++ CachorroTest.java
@@ -7,14 +7,13 @@ public class CachorroTest{
 	 */
 	public static void main(String[] args) {
 		
-		Cachorro pitbull = new Cachorro();//cria-se um objeto que faz referência à classe Cachorro
+		Cachorro pitbull = new Cachorro();
 		
-		pitbull.raca = "Pit Bull";//esse objeto deve acessar os atributos (variáveis) da classe Cachorro 
+		pitbull.raca = "Pit Bull";
 		pitbull.tamanho = 30;
-		pitbull.latir();//deve também chamar o método criado na classe Cachorro;
+		pitbull.latir();
 		
-		Cachorro viralata = new Cachorro();//pode-se ciar quantos objetos forem necessários
-		//e acessar seus (na verdade o da classe) atributos e métodos sempre dessa forma
+		Cachorro viralata = new Cachorro();
 		viralata.raca = "Vira-Lata";
 		viralata.tamanho = 40;
 		viralata.latir();
